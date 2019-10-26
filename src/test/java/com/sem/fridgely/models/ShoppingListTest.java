d64e947d84ecde36a56cc574535266a929b7d38e
cdanqian <c.dan.qian@gmail.com>
2019-10-26 12:13:23 -0700

    UT update

--- src/test/java/com/sem/fridgely/models/ShoppingListTest.java
+++ src/test/java/com/sem/fridgely/models/ShoppingListTest.java
@@ -2,6 +2,7 @@ package com.sem.fridgely.models;
 
 import com.sem.fridgely.manager.ShoppingListManager;
 import org.bson.Document;
+import org.bson.types.Binary;
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -15,23 +16,24 @@ public class ShoppingListTest {
     @Test
     public void testGetInJASON() throws JSONException {
         // Arrange
-
         JSONArray items = new JSONArray().put(new JSONObject()
                 .put("id", "123")
                 .put("qty", 10)
                 .put("unit", "g"));
         Document doc = new Document(ShoppingListManager.FIELD_ID, "123")
                 .append(ShoppingListManager.FIELD_NAME, "test list")
-                .append(ShoppingListManager.FIELD_USERID, "u123");
+                .append(ShoppingListManager.FIELD_USERID, "u123")
+                .append(ShoppingListManager.FIELD_ITEMS,new Binary(items.toString().getBytes()));
+
         JSONObject expected = new JSONObject().put("id", "123")
                 .put("name", "test list")
-                .put("userid", "u123");
+                .put("userid", "u123").put("items", items);
 
         // Action
         ShoppingList undertest = new ShoppingList(doc);
         JSONObject actual = undertest.getInJson();
 
-        assertEquals(actual.get("id"),"123");
+        assertEquals(expected.toString(),actual.toString());
 
     }
 }
